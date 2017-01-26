2df24a5b89e9863dae6ac0eae86dde9b758b7544
voltagegg <voltagegg@gmail.com>
2017-01-26 13:36:58 +0400

    Update tux_install.sh

--- tux_install.sh
+++ tux_install.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 ##DATE=`/bin/date '+%d%m'`
-###UPGRADE OS###
+###UPGRADE OS & DEL STEAM-RUNTIME###
 if [ -e /etc/arch-release ]; then 
-    sudo pacman -Syu &&
+    sudo pacman -Syu
     sudo pacman -S base-devel cmake gdb git sdl2 xdotool
 fi
-if [ -e /etc/debian_version ]; then 
-        sudo apt-get update &&
-        sudo apt-get install -y cmake g++ gdb git libsdl2-dev zlib1g-dev libxdo-dev
+if [ -e /etc/debian_version ]; then
+    sudo apt-get update
+    sudo apt-get install -y cmake g++ gdb git libsdl2-dev zlib1g-dev libxdo-dev
+        rm ~/.steam/ubuntu12_32/steam-runtime/i386/usr/lib/i386-linux-gnu/libstdc++.so.*
+        rm ~/.steam/ubuntu12_32/steam-runtime/i386/lib/i386-linux-gnu/libgcc_s.so.*
+        rm ~/.steam/ubuntu12_32/steam-runtime/amd64/lib/x86_64-linux-gnu/libgcc_s.so.*
+        rm ~/.steam/ubuntu12_32/steam-runtime/amd64/usr/lib/x86_64-linux-gnu/libstdc++.so.*
+        rm ~/.steam/ubuntu12_32/steam-runtime/i386/usr/lib/i386-linux-gnu/libxcb.so.*
 fi
 ###STEAM FIX###
 [ ! -d /tmp/dumps ] && sudo mkdir /tmp/dumps
@@ -25,13 +30,46 @@ sudo chmod -R 777 /home/$USER/.config/AimTux
 ###CLEAN INSTALL###
 cd /tmp
 [ -d /tmp/AimTux* ] && sudo rm -rf AimTux*
-###FACEIT###
-[ -f /tmp/faceit* ] && sudo rm faceit*
-[ -d /home/at/am_faceit ] && sudo rm -rf /home/at/am_faceit
-wget https://github.com/McSwaggens/AimTux/archive/faceit.zip && unzip faceit.zip
-mv /tmp/AimTux-faceit /home/at/am_faceit
-cd /home/at/am_faceit
-cmake .
-make -j 4
-echo "FINISH"
-
+###CASE###
+printf 'Select the version AimTux to install! 1)New; 2)Stable; 3)Faceit; 4)Install configs; 5)Cleaning configs; 6)Cleaning AimTux : '
+read BRANCH
+case $BRANCH in
+     1)
+          echo "Compiling AimTux default..."
+          [ -d /home/at/am_new ] && sudo rm -rf /home/at/am_new
+          sudo rm master.*
+          git clone https://github.com/McSwaggens/AimTux
+          mv /tmp/AimTux /home/at/am_new
+          cd /home/at/am_new
+          cmake .
+          make -j 4
+          echo "FINISH"
+          ;;
+     2)
+          echo "Эй! Это мой любимый серверный дистрибутив!"
+          ;;
+     3)
+          echo "Compiling AimTux for FACEIT..."
+          [ -d /home/at/am_faceit ] && sudo rm -rf /home/at/am_faceit
+          sudo rm faceit.*
+          wget https://github.com/McSwaggens/AimTux/archive/faceit.zip && unzip faceit.zip
+          mv /tmp/AimTux-faceit /home/at/am_faceit
+          cd /home/at/am_faceit
+          cmake .
+          make -j 4
+          echo "Finished compiling AimTux for FACEIT"
+          ;;
+     4)
+          echo "Эй! Это мой любимый серверный дистрибутив!"
+          ;;
+     5)
+          echo "Please enter a number!"
+          ;;
+     6)
+          echo "Эй! Это мой любимый серверный дистрибутив!"
+          ;;
+     *)
+          echo "Please enter a number!"
+          ;;
+esac
+echo "Script finished the job!"
